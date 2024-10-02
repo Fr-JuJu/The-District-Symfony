ace389382da464b8c7e4c92e4b1560b2044e15d9
Fr-JuJu <juliesueur70@gmail.com>
2024-10-02 15:20:33 +0200

    finir formu

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -16,10 +16,40 @@ use Symfony\Component\Mime\Email;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
     {
+
+        $contact = new Contact();
+
+        $form = $this->createForm(ContactFormType::class, $contact);
+
+        $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                $contact = $form->getData();
+                $entityManager->persist($contact);
+                $entityManager->flush();
+
+                $email = (new Email())
+                ->from('thedistrict@laposte.fr')
+                ->to('exemple@ex.com')
+                ->subject('Confirmation de contact')
+                ->text('Bonjour !
+                Nous avons bien reçu votre mail. Nous allons examiner votre demande et nous vous contacterons dans les plus brefs délais.
+                Cordialement,
+                l\'équipe The District.');
+
+                try {
+                    $mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+
+                }
+                return $this->redirectToRoute('app_index');
+            }
+
+        
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form
         ]);
     }
 }
