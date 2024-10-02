b95492c911974e33eded67423d5676c277ec2500
Fr-JuJu <juliesueur70@gmail.com>
2024-10-02 15:06:44 +0200

    mail contact à faire

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
